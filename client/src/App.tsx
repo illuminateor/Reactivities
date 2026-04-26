8645d109fd5b3c9fbe430fd185063f2cae05c22d
illuminateor <o.mudrak93@gmail.com>
2026-04-26 09:53:03 +0200

    Add client with material ui

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>('https://localhost:5001/api/activities')
+      .then((response) => setActivities(response.data));
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
