7999d1589085d5c63281f780672739936c18a9e8
illuminateor <o.mudrak93@gmail.com>
2026-07-22 18:09:52 +0200

    Add routing

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -2,14 +2,12 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Button, Container, MenuItem, MenuList } from '@mui/material';
+import { Container, MenuItem, MenuList } from '@mui/material';
 import { Group } from '@mui/icons-material';
+import { NavLink } from 'react-router';
+import MenuItemLink from '../shares/components/MenuItemLink';
 
-type Props = {
-  openForm: (id?: string) => void;
-};
-
-export default function NavBar({ openForm }: Props) {
+export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -29,7 +27,11 @@ export default function NavBar({ openForm }: Props) {
           >
             <MenuList>
               <Box>
-                <MenuItem sx={{ display: 'flex', gap: 2 }}>
+                <MenuItem
+                  component={NavLink}
+                  to="/"
+                  sx={{ display: 'flex', gap: 2 }}
+                >
                   <Group fontSize="large" />
                   <Typography variant="h4" style={{ fontWeight: 'bold' }}>
                     Reactivities
@@ -45,43 +47,15 @@ export default function NavBar({ openForm }: Props) {
               }}
             >
               <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
-                <MenuItem
-                  sx={{
-                    fontSize: '1.2rem',
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Activities
-                </MenuItem>
-                <MenuItem
-                  sx={{
-                    fontSize: '1.2rem',
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  About
-                </MenuItem>
-                <MenuItem
-                  sx={{
-                    fontSize: '1.2rem',
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Contact
-                </MenuItem>
+                <MenuItemLink to="/activities">Activities</MenuItemLink>
+                <MenuItemLink to="/createActivity">
+                  Create Activity
+                </MenuItemLink>
               </Box>
             </MenuList>
-            <Button
-              size="large"
-              variant="contained"
-              color="warning"
-              onClick={() => openForm()}
-            >
-              Create activity
-            </Button>
+            <MenuList>
+              <MenuItem>User menu</MenuItem>
+            </MenuList>
           </Toolbar>
         </Container>
       </AppBar>
--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,13 @@
 import { Box, Button, Paper, TextField, Typography } from '@mui/material';
 import type { FormEvent } from 'react';
 import { useActivities } from '../../../lib/hooks/useActivities';
+import { useNavigate, useParams } from 'react-router';
 
-type Props = {
-  closeForm: () => void;
-  activity?: Activity;
-};
-
-export default function ActivityForm({ closeForm, activity }: Props) {
-  const { updateActivity, createActivity } = useActivities();
+export default function ActivityForm() {
+  const { id } = useParams();
+  const { updateActivity, createActivity, activity, isLoadingActivity } =
+    useActivities(id);
+  const navigate = useNavigate();
 
   const handleSumbit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,16 +21,22 @@ export default function ActivityForm({ closeForm, activity }: Props) {
     if (activity) {
       data.id = activity.id;
       await updateActivity.mutateAsync(data as unknown as Activity);
-      closeForm();
+      navigate(`/activities/${activity.id}`);
     } else {
-      await createActivity.mutateAsync(data as unknown as Activity);
-      closeForm();
+      createActivity.mutate(data as unknown as Activity, {
+        onSuccess: (id) => {
+          navigate(`/activities/${id}`);
+        },
+      });
     }
   };
+
+  if (isLoadingActivity) return <Typography>Loading activity...</Typography>;
+
   return (
     <Paper sx={{ borderRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
-        Create Activity
+        {activity ? 'Edit activity' : 'Create activity'}
       </Typography>
       <Box
         component="form"
@@ -82,9 +87,7 @@ export default function ActivityForm({ closeForm, activity }: Props) {
           fullWidth
         />
         <Box sx={{ justifyContent: 'flex-end', display: 'flex', gap: 3 }}>
-          <Button color="inherit" onClick={closeForm}>
-            Cancel
-          </Button>
+          <Button color="inherit">Cancel</Button>
           <Button
             variant="contained"
             color="success"
