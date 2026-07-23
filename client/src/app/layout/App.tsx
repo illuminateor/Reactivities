fae37bc59b5e8cd24cc3e63e16d7bc234c96201c
illuminateor <o.mudrak93@gmail.com>
2026-07-23 18:05:17 +0200

    Improve UI/UX

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,16 +2,25 @@ import NavBar from './NavBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { Box } from '@mui/material';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
+import HomePage from '../../features/home/HomePage';
 
 function App() {
+  const location = useLocation();
+
   return (
     <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
       <CssBaseline />
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === '/' ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container maxWidth="xl" sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   );
 }
