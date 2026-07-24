d4f28613ea8ce24e8cfbc6358b3d8e0bc0a8d6f7
illuminateor <o.mudrak93@gmail.com>
2026-07-24 14:45:22 +0200

    Add mobx and counter

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -2,12 +2,15 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Container, MenuItem, MenuList } from '@mui/material';
+import { Container, LinearProgress, MenuItem, MenuList } from '@mui/material';
 import { Group } from '@mui/icons-material';
 import { NavLink } from 'react-router';
 import MenuItemLink from '../shares/components/MenuItemLink';
+import { useStore } from '../../lib/hooks/useStore';
+import { Observer } from 'mobx-react-lite';
 
 export default function NavBar() {
+  const { uiStore } = useStore();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -15,6 +18,7 @@ export default function NavBar() {
         sx={{
           backgroundImage:
             'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+          position: 'relative',
         }}
       >
         <Container maxWidth="xl">
@@ -51,6 +55,7 @@ export default function NavBar() {
                 <MenuItemLink to="/createActivity">
                   Create Activity
                 </MenuItemLink>
+                <MenuItemLink to="/counter">Counter</MenuItemLink>
               </Box>
             </MenuList>
             <MenuList>
@@ -58,6 +63,23 @@ export default function NavBar() {
             </MenuList>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() =>
+            uiStore.isLoading ? (
+              <LinearProgress
+                color="secondary"
+                sx={{
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 4,
+                }}
+              />
+            ) : null
+          }
+        </Observer>
       </AppBar>
     </Box>
   );
